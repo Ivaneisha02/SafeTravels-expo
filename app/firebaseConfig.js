d7a845c67f456de5e3b3ca466384c37b518962b5
Khendra Phillips <khendra.phillips@bison.howard.edu>
2025-04-16 15:55:52 -0400

    firbase database configuration

---
+++ app/firebaseConfig.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAYwFhxQ8BQu_OOI4xvlbw6AKCDtq6x4_c",
+  authDomain: "safetravels-ba48e.firebaseapp.com",
+  projectId: "safetravels-ba48e",
+  storageBucket: "safetravels-ba48e.firebasestorage.app",
+  messagingSenderId: "128702550576",
+  appId: "1:128702550576:web:1db5a27f32f2a0310993f5",
+  measurementId: "G-PVH0FYV07C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export services
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
